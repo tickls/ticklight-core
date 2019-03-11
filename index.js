8a975aedc8aaeb7e914ebba23635f892d9420f60
Evan Kluin <evan@tickls.com>
2019-03-11 19:13:30 +0100

    Added event

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const Error = require('./lib/error');
-const Service = require('./lib/service');
-const Plugin = require('./lib/plugin');
+'use strict';
+
 const Device = require('./lib/device');
+const TickleEvent = require('./lib/event');
+const Error = require('./lib/error');
+const Plugin = require('./lib/plugin');
+const Service = require('./lib/service');
+const SystemService = require('./lib/system_service');
 
 module.exports = {
+    Device,
+    TickleEvent,
     Error,
-    Service,
     Plugin,
-    Device
+    Service,
+    SystemService
 }
